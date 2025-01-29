3fd7fab5d5c9df6af12383f52aef5c26a427f60b
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-01-29 11:23:13 +0100

    feat: fetch relevant data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import './App.css'
+import { useEffect, useState } from "react";
+import { getAccomodations } from "./util/apiCalls";
 
 function App() {
+  const [accomodations, setAccomodations] = useState([])
+
+  useEffect(() => {
+    getAccomodations(
+      (r) => setAccomodations(r),
+      (err) => console.log(err)
+    );
+  }, []);
 
 
   return (
     <>
-     <h1>Accomodation</h1>
+      <h1>React with Vite</h1>
+      {accomodations.map((a) => {
+        return (
+          <div key={a.id}>
+            <img src={a.image}></img>
+            <p>{a.title}</p>
+            <p>Capacity: {a.capacity}</p>
+            {a.beachDistanceInMeters && <p>Beach Distance: {a.beachDistanceInMeters}m</p>}
+
+          </div>
+        );
+      })}
     </>
   )
 }
---
+++ src/util/apiCalls.js
@@ -0,0 +1,15 @@
+export const getAccomodations = async (onSuccess, onError) => {
+  try {
+    const response = await fetch("https://api.adriatic.hr/test/accommodation");
+
+    if (!response.ok) {
+      const errorMessage = `Error: ${response.status} ${response.statusText}`;
+      throw new Error(errorMessage);
+    }
+    const r = await response.json();
+    onSuccess(r);
+  } catch (error) {
+    onError(error instanceof Error ? error : new Error("Unknown error occurred"));
+  }
+};
+
