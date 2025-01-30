4cd6fe6083bbfdec46907b353787f969bd8ac6be
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-01-30 21:35:55 +0100

    chore: add datepicker

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,45 @@
+import { useState, useEffect } from 'react'
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
 export default function Filters(props) {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+
+  useEffect( () => {
+    console.log(startDate, endDate)
+  })
+  
+  
   return (
     <>
-      <form  onChange={props.onFiltering} className="mb-10">
+      <form onChange={props.onFiltering} className="mb-10">
         <div className="flex gap-2 p-1 m-1 bg-indigo-50 rounded-md items-center content-start">
-          <input type="number" name="capacity" className="border-1" min="1" defaultValue="1" />
+          <DatePicker
+            selected={startDate}
+            onChange={onChange}
+            startDate={startDate}
+            endDate={endDate}
+            selectsRange
+            minDate={new Date()}
+          />
+          <label htmlFor="Capacity">Reservation dates</label>
+        </div>
+
+        <div className="flex gap-2 p-1 m-1 bg-indigo-50 rounded-md items-center content-start">
+          <input
+            type="number"
+            name="capacity"
+            className="border-1"
+            min="1"
+            defaultValue="1"
+          />
           <label htmlFor="Capacity">Number of Guests</label>
         </div>
 
@@ -38,7 +74,14 @@ export default function Filters(props) {
         </div>
 
         <div className="flex gap-2 p-1 m-1 bg-indigo-50 rounded-md items-center content-start">
-          <input type="number" name="max_price" className="border-1" step="10" min="0" defaultValue="0" />
+          <input
+            type="number"
+            name="max_price"
+            className="border-1"
+            step="10"
+            min="0"
+            defaultValue="0"
+          />
           <label htmlFor="max_price">Maximum price</label>
         </div>
       </form>
