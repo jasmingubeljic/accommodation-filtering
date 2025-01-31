2e595bf0b62ff915514f523e348f8871553a6303
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-01-31 10:09:04 +0100

    refactor: move filters related code to Filter component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,11 +1,86 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import DatePicker from "react-datepicker";
-
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function Filters(props) {
+  const [filters, setFilters] = useState({});
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(null);
+
+  useEffect(() => {
+    /* Filtering logic */
+    const fa = props.accomodations.filter((a) => {
+      let conditionsMatchingArray = [];
+      if (filters.selIntervalStart && filters.selIntervalEnd) {
+        const matchesAvailableDates = [];
+        for (const obj of a.availableDates) {
+          if (
+            filters.selIntervalStart >= new Date(obj.intervalStart) &&
+            filters.selIntervalEnd <= new Date(obj.intervalEnd)
+          ) {
+            matchesAvailableDates.push(true);
+          }
+        }
+        conditionsMatchingArray.push(
+          matchesAvailableDates.some((value) => value === true)
+        );
+      }
+      if (filters.capacity) {
+        conditionsMatchingArray.push(filters.capacity <= a.capacity);
+      }
+      if (filters.airConditioning) {
+        conditionsMatchingArray.push(a.amenities.airConditioning);
+      }
+      if (filters.parkingSpace) {
+        conditionsMatchingArray.push(a.amenities.airConditioning);
+      }
+      if (filters.pets) {
+        conditionsMatchingArray.push(a.amenities.pets);
+      }
+      if (filters.pool) {
+        conditionsMatchingArray.push(a.amenities.pool);
+      }
+      if (filters.wifi) {
+        conditionsMatchingArray.push(a.amenities.wifi);
+      }
+      if (filters.tv) {
+        conditionsMatchingArray.push(a.amenities.tv);
+      }
+      if (filters.maxPrice) {
+        if (filters.selIntervalStart && filters.selIntervalEnd) {
+          // take dates into account and prices related to dates
+          const withinPriceRangeArr = [];
+          for (const obj of a.pricelistInEuros) {
+            if (
+              filters.selIntervalStart >= new Date(obj.intervalStart) &&
+              filters.selIntervalEnd <= new Date(obj.intervalEnd)
+            ) {
+              for (const obj of a.pricelistInEuros) {
+                withinPriceRangeArr.push(filters.maxPrice <= obj.pricePerNight);
+              }
+            }
+          }
+          conditionsMatchingArray.push(
+            withinPriceRangeArr.some((value) => value === false)
+          );
+        } else {
+
+          // if date range is not selected, we check if condition meets at least one of the season prices
+          const withinPriceRangeArr = [];
+          for (const obj of a.pricelistInEuros) {
+            withinPriceRangeArr.push(filters.maxPrice <= obj.pricePerNight);
+          }
+          conditionsMatchingArray.push(
+            withinPriceRangeArr.some((value) => value === false)
+          );
+        }
+      }
+
+      return conditionsMatchingArray.every((val) => val === true);
+    }); // ---- end of the .filter() ----
+    console.log("filters: ", filters);
+    props.onSetFilteredAccomodations(fa);
+  }, [filters, props.accomodations]);
 
   const onChange = (dates) => {
     const [start, end] = dates;
@@ -13,14 +88,59 @@ export default function Filters(props) {
     setEndDate(end);
   };
 
+  const onFilteringHandler = useCallback((event) => {
+    // handle range picker
+    if (!event.target) {
+      console.log("event: ", event);
+      if (event.length > 1) {
+        setFilters((prevState) => {
+          return {
+            ...prevState,
+            selIntervalStart: event[0],
+            selIntervalEnd: event[1],
+          };
+        });
+      }
+      return;
+    }
+
+    // handle remaining filters
+    const name = event.target.name;
+    console.log("event:   ", event);
+
+    if (event.target.type == "checkbox" && event.target.checked) {
+      setFilters((prevState) => {
+        return {
+          ...prevState,
+          [name]: event.target.checked,
+        };
+      });
+    } else {
+      setFilters((prevState) => {
+        const obj = { ...prevState };
+        delete obj[name];
+        return obj;
+      });
+    }
+
+    if (event.target.type == "number") {
+      setFilters((prevState) => {
+        return {
+          ...prevState,
+          [name]: +event.target.value,
+        };
+      });
+    }
+  }, []);
+
   return (
     <>
-      <form onChange={props.onFiltering} className="mb-10">
+      <form onChange={onFilteringHandler} className="mb-10">
         <div className="flex gap-2 p-1 m-1 bg-indigo-50 rounded-md items-center content-start">
           <DatePicker
             selected={startDate}
             onChange={(event) => {
-              props.onFiltering(event);
+              onFilteringHandler(event);
               onChange(event);
             }}
             startDate={startDate}
--- src/components/pages/AccomodationPage.jsx
+++ src/components/pages/AccomodationPage.jsx
@@ -1,11 +1,10 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 import { getAccomodations } from "../../util/apiCalls";
 import { Link } from "react-router-dom";
 import Filters from "../Filter/Filter";
 
 export default function AccomodationPage() {
   const [accomodations, setAccomodations] = useState([]);
-  const [filters, setFilters] = useState({});
   const [filteredAccomodations, setFilteredAccomodations] = useState([]);
 
   useEffect(() => {
@@ -18,137 +17,13 @@ export default function AccomodationPage() {
     );
   }, []);
 
-  useEffect(() => {
-    
-    /* Filtering logic */
-    const fa = accomodations.filter((a) => {
-
-      let conditionsMatchingArray = [];
-      if(filters.selIntervalStart && filters.selIntervalEnd) {
-        const matchesAvailableDates = []
-        for (const obj of a.availableDates) {
-          if(
-            filters.selIntervalStart >= new Date(obj.intervalStart) &&
-            filters.selIntervalEnd <= new Date(obj.intervalEnd)
-          ) {
-            matchesAvailableDates.push(true)
-          }
-        }
-        conditionsMatchingArray.push(
-          matchesAvailableDates.some(value => value === true)
-        )
-      }
-      if (filters.capacity) {
-        conditionsMatchingArray.push(filters.capacity <= a.capacity);
-      }
-      if (filters.airConditioning) {
-        conditionsMatchingArray.push(a.amenities.airConditioning);
-      }
-      if (filters.parkingSpace) {
-        conditionsMatchingArray.push(a.amenities.airConditioning);
-      }
-      if (filters.pets) {
-        conditionsMatchingArray.push(a.amenities.pets);
-      }
-      if (filters.pool) {
-        conditionsMatchingArray.push(a.amenities.pool);
-      }
-      if (filters.wifi) {
-        conditionsMatchingArray.push(a.amenities.wifi);
-      }
-      if (filters.tv) {
-        conditionsMatchingArray.push(a.amenities.tv);
-      }
-      if(filters.maxPrice) {
-        if(filters.selIntervalStart && filters.selIntervalEnd) {
-          console.log('inside if')
-          // take dates into account and prices date related
-          const withinPriceRangeArr = []
-          for (const obj of a.pricelistInEuros) {
-            if(
-              filters.selIntervalStart >= new Date(obj.intervalStart) &&
-              filters.selIntervalEnd <= new Date(obj.intervalEnd)
-              
-            ) {
-              for (const obj of a.pricelistInEuros) {
-                withinPriceRangeArr.push(filters.maxPrice <= obj.pricePerNight) 
-              }
-            }
-          }
-          conditionsMatchingArray.push(
-            withinPriceRangeArr.some(value => value === false)
-          )
-        } else {    
-          console.log('inside else')
-
-          // if date range is not selected, we check if condition meets at least one of the season prices 
-          const withinPriceRangeArr = []
-          for (const obj of a.pricelistInEuros) {
-            withinPriceRangeArr.push(filters.maxPrice <= obj.pricePerNight) 
-          }
-          conditionsMatchingArray.push(
-            withinPriceRangeArr.some(value => value === false)
-          )
-        }
-      }
-      
-      return conditionsMatchingArray.every(val => val === true)
-    }); // ---- end of the .filter() ----
-    console.log('filters: ', filters)
-    setFilteredAccomodations(fa);
-  }, [filters, accomodations]);
-
-
-  const onFilteringHandler = useCallback((event) => {
-
-    // handle range picker
-    if (!event.target) {
-      console.log('event: ', event)
-      if (event.length > 1) {
-        setFilters((prevState) => {
-          return {
-            ...prevState,
-            selIntervalStart: event[0],
-            selIntervalEnd: event[1]
-          };
-        });
-      }
-      return;
-    }
-
-    // handle remaining filters
-    const name = event.target.name;
-    console.log("event:   ", event);
-
-    if (event.target.type == "checkbox" && event.target.checked) {
-      setFilters((prevState) => {
-        return {
-          ...prevState,
-          [name]: event.target.checked,
-        };
-      });
-    } else {
-      setFilters((prevState) => {
-        const obj = { ...prevState };
-        delete obj[name];
-        return obj;
-      });
-    }
-
-    if (event.target.type == "number") {
-      setFilters((prevState) => {
-        return {
-          ...prevState,
-          [name]: +event.target.value,
-        };
-      });
-    }
-  }, []);
-
-
+  
   return (
     <>
-      <Filters onFiltering={onFilteringHandler} />
+      <Filters
+        accomodations={accomodations}
+        onSetFilteredAccomodations={(fa) => setFilteredAccomodations(fa)}
+      />
       <div className="grid grid-cols-2 gap-3 md:grid-cols-4 md:gap-5">
         {filteredAccomodations.map((a) => {
           return (
