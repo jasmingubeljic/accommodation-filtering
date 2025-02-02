b079e942d612e2f0eb10ae97ac1f6b65f1bcc091
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-02-02 10:51:26 +0100

    feat: add personal brand

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -3,7 +3,8 @@ import { Outlet } from 'react-router-dom'
 export default function Container () {
 
     return <>
-        <h1 className="uppercase text-gray-800 tracking-wide">Accommodations</h1>
+        <h1 className="text-lg uppercase text-gray-800 tracking-wide">Accommodations</h1>
+        <p className="text-sm text-gray-400">Jasmin Gubeljić ~ Task</p>
         <Outlet />
     </>
 
